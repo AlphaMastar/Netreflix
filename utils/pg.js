0e0c89909c6c99fc7fbe6e98e5c7005eaeaefe68
anyiyun from code-server <artreflix@outlook.com>
2023-12-28 23:30:59 +0800

    # 设置.env全局文件，配置全局优化

--- utils/pg.js
+++ utils/pg.js
@@ -1,16 +1,8 @@
-const mysql = require('mysql')
-require('dotenv').config();
+const { Pool } = require('pg')
+require('dotenv').config()
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,         
-    password: process.env.MYSQL_PASSWORD,     
-    database: process.env.MYSQL_DATABASE,
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
+const pool = new Pool({
+  connectionString: "postgres://default:Xk4jdycLfU5O@ep-wandering-cell-42846555-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
 })
 
 function MysqlExcute(sql, data){
