697a7aa39b70545d729ec01c471784b9b1957e16
AlphaMastar <Artreflix@outlook.com>
2023-12-29 21:34:53 +0800

    #url参数处理优化

--- controller/imageColorController.js
+++ controller/imageColorController.js
@@ -1,30 +1,34 @@
-let imageColor = require('../service/imageColor');
-let imageMainColor = require('../utils/imageMainColor');
+const imageColor = require('../service/imageColor');
+const imageMainColor = require('../utils/imageMainColor');
+const urlRegex = /^(((ht|f)tp?):\/\/)?([^!@#$%^&*?.\s-]([^!@#$%^&*?.\s]{0,63}[^!@#$%^&*?.\s])?\.)+[a-z]{2,6}\/?/;
 
 module.exports = {
     async imageColorController(req, res) {
         let param = req.query.url;
-        let result = await imageColor.imageColorQueryByUrl(param);
-        if (result.length == 0) {
-            imageMainColor.getImageColorRGB(param)
-            .then((HexColor) => {
-                this.jsonWrite(res, HexColor);
-                imageColor.imageColorAdd([param, HexColor.RGB]);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        if (urlRegex.test(param), param != undefined) {
+            let result = await imageColor.imageColorQueryByUrl(param);
+            if (result.length == 0) {
+                imageMainColor.getImageColorRGB(param)
+                .then((HexColor) => {
+                    this.jsonWrite(res, HexColor);
+                    imageColor.imageColorAdd([param, HexColor.RGB]);
+                })
+                .catch((err) => {
+                    this.jsonWrite(res, err);
+                })
+            } else {
+                let HexColor = {RGB: result[0].RGB};
+                this.jsonWrite(res, HexColor); 
+            };
         } else {
-            let HexColor = {RGB: result[0].RGB};
-            this.jsonWrite(res, HexColor); 
+            this.jsonWrite(res, {"参数不合法": param})
         };
-        
     },
     jsonWrite (res, ret) {
         if(typeof ret === 'undefined') {
             res.json({
-                code:'1',
-                msg: '操作失败'
+                code:'500',
+                Message: '操作失败'
             });
         } else {res.send(ret);}
     }
